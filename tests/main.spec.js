10924167628233d5ea35ad733c0dfdfe645762a6
Robert Jackson <robert.w.jackson@me.com>
2013-11-27 16:50:30 -0500

    Setup `npm test` script.
    This commit adds `jasmine-node` as to `devDependencies` and removes the
    `tests/runner.sh` script. Now tests can be run from the projects root
    directory with `npm test`.
    
    `npm test` is the default test script used by Travis for Node projects.

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -5,7 +5,7 @@ var path = require('path');
 describe("ember-template-compiler tests", function() {
 
   it("compiles down a handlebars template", function() {
-    var template = fs.readFileSync(path.join('file-system', 'app', 'templates', 'foo.handlebars')).toString();
+    var template = fs.readFileSync(path.join(path.dirname(fs.realpathSync(__filename)),'file-system', 'app', 'templates', 'foo.handlebars')).toString();
     var result = sut.precompile(template).toString();
     expect(result).toContain("outlet");
   });
